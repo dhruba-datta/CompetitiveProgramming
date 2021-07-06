d57a6d37d1b0a6e2d68b63665760104c4e6837c7
Dhruba Datta <dhrubadattaanjan@gmail.com>
2021-07-06 21:23:47 +0530

    1.6 solved

---
+++ Week 01/06.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < 1; i++)
+    {
+        s[i] = toupper(s[i]);
+    }
+    cout << s;
+
+    return 0;
+}
