a6e622c0ca0c253ef8ddcd6d953266c123b6f1c7
Dhruba Datta <dhrubadattaanjan@gmail.com>
2021-07-08 20:18:07 +0530

    1.9 solved

---
+++ Week 01/09.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, x, a = 0, b = 0, c = 0;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cin >> x;
+            if (j == 0)
+            {
+                a = a + x;
+            }
+            else if (j == 1)
+            {
+                b = b + x;
+            }
+            else
+            {
+                c = c + x;
+            }
+        }
+    }
+
+    if (a == 0 && b == 0 && c == 0)
+    {
+        cout << "YES";
+    }
+    else
+        cout << "NO";
+
+    return 0;
+}
