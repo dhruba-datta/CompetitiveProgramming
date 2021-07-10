386ccfa688a5bd1f4931962dcf92b7a06abae3ba
Dhruba Datta <dhrubadattaanjan@gmail.com>
2021-07-11 03:58:47 +0530

    2.6 solved

--- Week 02/06.cpp
+++ Week 02/06.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main()
 {
+    // solve 01
+    /*
     int s1, s2, s3, s4, count = 0;
     cin >> s1 >> s2 >> s3 >> s4;
     if (s1 == s2 || s1 == s3 || s1 == s4)
@@ -19,5 +21,23 @@ int main()
     }
 
     cout << count;
+    */
+
+    // solve 02
+    int a[4], count = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a, a + 4);
+    for (int i = 0; i < 3; i++)
+    {
+        if (a[i] == a[i + 1])
+        {
+            count++;
+        }
+    }
+    cout << count;
+
     return 0;
 }
